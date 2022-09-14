6d7e77a337d75697ed9c0a0e1a7cca40295d9bf8
mansa susa <mansasusa18@gmail.com>
2022-09-14 11:12:22 -0400

    run linter files and fixed linter errors

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
+// variable declarations to get nodes//
+const menuBox = document.getElementById('menuBox');
+const menuDialog = menuBox.querySelector('#menuDialog');
+const menuBtn = menuDialog.querySelector('.hamburger');
+const closeBtn = menuDialog.querySelector('.times');
+const menuLinks = menuDialog.querySelector('.nav-links');
+
 // displayMenu function declaration //
-function displayMenu () {
+function displayMenu() {
   menuBox.classList.toggle('menuDialog');
 }
 
 // closeMenu function declaration //
-function closeMenu () {
+function closeMenu() {
   menuBox.classList.toggle('menuDialog');
 }
 
-//variable declarations to get nodes//
-const menuBox = document.getElementById('menuBox');       
-const menuDialog = menuBox.querySelector('#menuDialog');
-const menuBtn = menuDialog.querySelector(".hamburger");
-const closeBtn = menuDialog.querySelector(".times");
-const menuLinks = menuDialog.querySelector(".nav-links")
+menuBtn.addEventListener('click', displayMenu); // hamburger event listener //
 
-menuBtn.addEventListener('click', displayMenu);  // hamburger event listener //
-
-closeBtn.addEventListener('click', closeMenu);   // times event listener //
+closeBtn.addEventListener('click', closeMenu); // times event listener //
 
 // Event listener for each menu link //
 Array.from(menuLinks.children).forEach((child) => child.addEventListener('click', closeMenu));
