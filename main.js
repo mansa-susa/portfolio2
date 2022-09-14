cf71c1f887254151b6402b3004d9daec12ba0c34
mansa susa <mansasusa18@gmail.com>
2022-09-14 05:55:40 -0400

    Add event listeners for the menu buttons and links

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const menuBox = document.getElementById('menuBox');
+const menuBox = document.getElementById('menuBox');       //variable declarations to get nodes//
 const menuDialog = menuBox.querySelector('#menuDialog');
 const menuBtn = menuDialog.querySelector(".hamburger");
 const closeBtn = menuDialog.querySelector(".times");
 const menuLinks = menuDialog.querySelector(".nav-links")
 
-menuBtn.addEventListener('click', displayMenu);
+menuBtn.addEventListener('click', displayMenu);  // hamburger event listener //
 
-closeBtn.addEventListener('click', closeMenu);
+closeBtn.addEventListener('click', closeMenu);   // times event listener //
 
+// Event listener for each menu link //
 Array.from(menuLinks.children).forEach((child) => child.addEventListener('click', closeMenu));
