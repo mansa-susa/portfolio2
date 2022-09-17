7ada953873e82508221252f5e492d5ecfd94e92d
mansa susa <mansasusa18@gmail.com>
2022-09-17 14:45:40 -0400

    Add mobile popup window styles

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ const menuBtn = menuDialog.querySelector('.hamburger');
 const closeBtn = menuDialog.querySelector('.times');
 const menuLinks = menuDialog.querySelector('.nav-links');
 
-let workSection = document.createElement('section');
-let workSectionTitle = document.createElement('h2');
-let workSectionRule = document.createElement('hr');
+const mainSection = document.querySelector('.main-section')
+const workSection = document.createElement('section');
+const workSectionTitle = document.createElement('h2');
+const workSectionRule = document.createElement('hr');
 let cardNumbers = 6;
-let portfolioCard = [{
+
+//Representing each cards with an object stored in an array //
+const portfolioCards = [{
   cardTitile: 'Multi-Post Stories Gain+Glory',
+  cardDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
   technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'],
   button: 'See Project'
 },
@@ -56,30 +60,39 @@ function closeMenu() {
   menuBox.classList.toggle('menuDialog');
 }
 
-workSection.className= 'work-section';
-workSectionTitle.className= 'work-section-title'
-workSection.appendChild(workSectionTitle,);
-workSection.appendChild(workSectionRule);
+workSection.className= 'work-section';  // Add class to the work section //
+workSectionTitle.className= 'work-section-title'; // Add class to work sectoin title //
+workSectionTitle.id = 'Portfolio'; // Add an id to the work section //
+workSectionTitle.innerText = 'My Recent Works';
+workSection.append(workSectionTitle, workSectionRule);
 
-portfolioCard.forEach(project => {
+portfolioCards.forEach(card => {
    workSection.innerHTML += `
           <article class="card-portfolio">
           <div class="img-placeholder"></div>
           <div class="project-description">
-            <h3>${project['cardTitile']}</h3>
+            <h3>${card['cardTitile']}</h3>
             <ul class="tech-skills">
-              <li>${project['technologies'][0]}</li>
-              <li>${project['technologies'][1]}</li>
-              <li>${project['technologies'][2]}</li>
-              <li>${project['technologies'][3]}</li>
+              <li>${card['technologies'][0]}</li>
+              <li>${card['technologies'][1]}</li>
+              <li>${card['technologies'][2]}</li>
+              <li>${card['technologies'][3]}</li>
             </ul>
-            <button type="button" class="project-button">${project['button']}}</button>
+            <button type="button" class="project-button">${card['button']}</button>
           </div>
         </article>`;
 })
 
-document.body.appendChild(workSection);
+mainSection.after(workSection);
 
+
+projectButtons = document.querySelectorAll('.work-section .project-button'); 
+projectButtons.forEach(button => button.addEventListener('click', displayPopup));
+
+/*for (let  i = 0; i < portfolioCards.length; i++) {
+  projectButtons[i].addEventListener('click', displayPopup)
+}
+*/
 
 menuBtn.addEventListener('click', displayMenu); // hamburger event listener //
 
@@ -87,3 +100,23 @@ closeBtn.addEventListener('click', closeMenu); // times event listener //
 
 // Event listener for each menu link //
 Array.from(menuLinks.children).forEach((child) => child.addEventListener('click', closeMenu));
+
+ function displayPopup () {
+    const popupWindow = document.createElement('div');
+    popupWindow.className = 'project-popup';
+    popupWindow.innerHTML += `
+                <img src="imgs/popup-mobile.png" alt="">
+                <h2 class="popup-title"> Keeping track of hundreds of components </h2>
+                <ul class="popup-technologies">
+                  <li>Ruby on rails</li>
+                  <li>css</li>
+                  <li>Javascript</li>
+                </ul>
+                <p class="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p>
+                <div>
+                  <button>See Live  <img src="imgs/live_icon.png" id="live-icon"></button>
+                  <button>See Source <i class="fab fa-github"></i></button>
+                </div>
+           `;
+    document.body.appendChild(popupWindow);
+ }
